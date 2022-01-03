b4cfcd3940aa352310557e1fa6f61fee696eae4c
BHKim <byeongho.kim@mytrum.com>
2022-01-03 11:52:14 +0900

    add content

---
+++ docs/src/.vuepress/components/EmbedVideo.vue
@@ -0,0 +1,6 @@
+<template>
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/0dA1Pldtbxw?start=58" title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen></iframe>
+</template>
--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -1,92 +1,106 @@
-const { description } = require('../../package')
+const {description} = require('../../package')
 
 module.exports = {
-  /**
-   * Ref：https://v1.vuepress.vuejs.org/config/#title
-   */
-  title: 'Vuepress Docs Boilerplate',
-  /**
-   * Ref：https://v1.vuepress.vuejs.org/config/#description
-   */
-  description: description,
+    /**
+     * Ref：https://v1.vuepress.vuejs.org/config/#title
+     */
+    title: 'Vuepress Docs Boilerplate',
+    /**
+     * Ref：https://v1.vuepress.vuejs.org/config/#description
+     */
+    description: description,
 
-  /**
-   * Extra tags to be injected to the page HTML `<head>`
-   *
-   * ref：https://v1.vuepress.vuejs.org/config/#head
-   */
-  head: [
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
-  ],
-
-  /**
-   * Theme configuration, here is the default theme configuration for VuePress.
-   *
-   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
-   */
-  themeConfig: {
-    repo: '',
-    editLinks: false,
-    docsDir: '',
-    editLinkText: '',
-    lastUpdated: true,
-    sidebarDepth: 3,
-    nav: [
-      {
-        text: 'Summary',
-        link: '/summary/'
-      },
-      {
-        text: 'Experience',
-        link: '/experience/'
-      },
-      {
-        text: 'Learning',
-        link: '/learning/'
-      },
-      {
-        text: 'Know-how',
-        link: '/know-how/'
-      },
+    /**
+     * Extra tags to be injected to the page HTML `<head>`
+     *
+     * ref：https://v1.vuepress.vuejs.org/config/#head
+     */
+    head: [
+        ['meta', {name: 'theme-color', content: '#3eaf7c'}],
+        ['meta', {name: 'apple-mobile-web-app-capable', content: 'yes'}],
+        ['meta', {name: 'apple-mobile-web-app-status-bar-style', content: 'black'}]
     ],
-    sidebar: {
-      '/summary/': [
-        {
-          title: 'Summary',
-          collapsable: false,
-          children: [
-              '',
-              'why-fe-developer',
-          ]
-        },
-        {
-          title: 'Experience',
-          path: '/experience/'
-        }
-      ],
-      '/experience/': [
-        {
-          title: 'Experience',
-          collapsable: false,
-          children: [
-            '',
-            'workflow-editor',
-              'auto-campaign',
-              'admin',
 
-          ]
+    /**
+     * Theme configuration, here is the default theme configuration for VuePress.
+     *
+     * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+     */
+    themeConfig: {
+        repo: '',
+        editLinks: false,
+        docsDir: '',
+        editLinkText: '',
+        lastUpdated: true,
+        sidebarDepth: 3,
+        nav: [
+            {
+                text: 'Summary',
+                link: '/summary/'
+            },
+            {
+                text: 'Experience',
+                link: '/experience/'
+            },
+            {
+                text: 'Learning',
+                link: '/learning/'
+            },
+            {
+                text: 'Know-how',
+                link: '/know-how/'
+            },
+        ],
+        sidebar: {
+            '/summary/': [
+                {
+                    title: 'Summary',
+                    collapsable: false,
+                    children: [
+                        '',
+                        'why-fe-developer',
+                    ]
+                },
+                {
+                    title: 'Experience',
+                    path: '/experience/'
+                }
+            ],
+            '/experience/': [
+                {
+                    title: 'Experience',
+                    collapsable: false,
+                    children: [
+                        '',
+                        'workflow-editor',
+                        'auto-campaign',
+                        'admin',
+                    ]
+                }
+            ],
+            '/learning/': [
+                {
+                    title: 'Learning',
+                    collapsable: false,
+                    children: [
+                        '',
+                        'data-structure',
+                        'alogrithm',
+                        'designpattern',
+                        'flowchart',
+                        'http',
+                        'htmlcssjs',
+                    ]
+                }
+            ]
         }
-      ],
-    }
-  },
+    },
 
-  /**
-   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
-   */
-  plugins: [
-    '@vuepress/plugin-back-to-top',
-    '@vuepress/plugin-medium-zoom',
-  ]
+    /**
+     * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+     */
+    plugins: [
+        '@vuepress/plugin-back-to-top',
+        '@vuepress/plugin-medium-zoom',
+    ]
 }
